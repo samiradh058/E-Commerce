62849947ea9de38b7060240aa43d4d72281c1e6e
samiradh058 <adhikarisamir68@gmail.com>
2025-03-17 00:55:07 +0545

    feat: forget password functionality

---
+++ server/src/utils/send-mail.mjs
@@ -0,0 +1,25 @@
+import nodemailer from "nodemailer";
+
+export const sendEmail = async (to, subject, text) => {
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    text,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.error("Email sending error:", error);
+    throw new Error("Failed to send email");
+  }
+};
