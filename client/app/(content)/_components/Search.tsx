2dff85c41a2398b6cab17100dd722aba71bf7a7e
samiradh058 <adhikarisamir68@gmail.com>
2025-03-18 00:38:30 +0545

    fix: search from query param

--- client/app/(content)/_components/Search.tsx
+++ client/app/(content)/_components/Search.tsx
@@ -1,12 +1,43 @@
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 
 export default function Search() {
+  const [searchString, setSearchString] = useState("");
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const query = searchParams.get("search");
+    if (query) {
+      setSearchString(query);
+    } else {
+      setSearchString("");
+    }
+  }, [searchParams]);
+
+  function handleSearch(e: React.FormEvent) {
+    e.preventDefault();
+    if (searchString.trim()) {
+      const params = new URLSearchParams(window.location.search);
+      params.set("search", searchString);
+
+      router.push(`${pathname}?${params.toString()}`);
+    }
+  }
+
   return (
-    <div className="flex items-center justify-center p-4">
-      <form className="w-full max-w-md">
+    <div className="flex items-center justify-center">
+      <form className="w-full max-w-md" onSubmit={handleSearch}>
         <div className="flex items-center bg-white rounded-full shadow-md overflow-hidden">
           <input
             type="text"
+            value={searchString}
+            onChange={(e) => setSearchString(e.target.value)}
             className="w-full h-10 px-4 py-2 text-gray-700 focus:outline-none"
             placeholder="Search for products..."
           />
--- client/app/(content)/page.tsx
+++ client/app/(content)/page.tsx
@@ -1,9 +1,19 @@
 import Products from "./_components/Products";
+import Search from "./_components/Search";
 
 export default async function Main() {
   return (
     <div className="flex-grow p-6">
-      <h2 className="text-2xl font-bold mb-6 text-center">Available Items:</h2>
+      <div className="flex justify-between items-center mb-8 relative">
+        <h2 className="text-2xl font-bold text-center flex-grow">
+          Available Items:
+        </h2>
+
+        <div className="flex justify-end absolute right-0">
+          <Search />
+        </div>
+      </div>
+
       <Products />
     </div>
   );
